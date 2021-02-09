d225137feda9325af000f58683072a5b22ec7534
cuizhigang <cuizg2015fly@163.com>
2021-02-09 08:50:18 +0800

    15. 3数之和

--- src/main/java/top100/p0001_two_sum/Solution.java
+++ src/main/java/top100/p0001_two_sum/Solution.java
@@ -6,23 +6,22 @@ import java.util.Map;
 /**
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
  * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
- *
+ * <p>
  * 示例:
  * 给定 nums = [2, 7, 11, 15], target = 9
  * 因为 nums[0] + nums[1] = 2 + 7 = 9
  * 所以返回 [0, 1]
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/two-sum
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
- *
+ * <p>
  * 这题这正常的思路是双层for循环遍历两次nums（如twoSum1），枚举出所有可能的和看是否有符合条件的，此时
- *      时间复杂度 O(n²) ：遍历2次
- *      空间复杂度 O(1) ：只有i，j两个变量
+ * 时间复杂度 O(n²) ：遍历2次
+ * 空间复杂度 O(1) ：只有i，j两个变量
  * 优化的思路是以空间换时间，使用hash来存储已遍历过的值，因为hash的时间复杂度是O(1)，所以会优化整体的时间复杂度，此时
- *      时间复杂度 O(n) ：遍历1次
- *      空间复杂度 O(n) ：hash的空间复杂度
- *
+ * 时间复杂度 O(n) ：遍历1次
+ * 空间复杂度 O(n) ：hash的空间复杂度
  */
 class Solution {
     public int[] twoSum(int[] nums, int target) {
@@ -30,7 +29,7 @@ class Solution {
         map.put(nums[0], 0);
         for (int i = 1; i < nums.length; i++) {
             if (map.containsKey(target - nums[i])) {
-                return new int[] {i, map.get(target - nums[i])};
+                return new int[]{i, map.get(target - nums[i])};
             }
             map.put(nums[i], i);
 
@@ -40,6 +39,7 @@ class Solution {
 
     /**
      * 暴力解法：双层for循环
+     *
      * @param nums
      * @param target
      * @return
@@ -49,7 +49,7 @@ class Solution {
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
-                    return new int[] {i, j};
+                    return new int[]{i, j};
                 }
             }
         }
---
+++ src/main/java/top100/p0015_3sum/Solution.java
@@ -0,0 +1,111 @@
+package top100.p0015_3sum;
+
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+/**
+ * 15. 三数之和 (normal)
+ * <p>
+ * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
+ * <p>
+ * 注意：答案中不可以包含重复的三元组。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [-1,0,1,2,-1,-4]
+ * 输出：[[-1,-1,2],[-1,0,1]]
+ * 示例 2：
+ * <p>
+ * 输入：nums = []
+ * 输出：[]
+ * 示例 3：
+ * <p>
+ * 输入：nums = [0]
+ * 输出：[]
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 0 <= nums.length <= 3000
+ * -105 <= nums[i] <= 105
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/3sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ *
+ * - 关键字：不可以包含重复
+ * - 模式识别：利用排序避免重复答案
+ * - 降低复杂度变成twoSum
+ * - 利用双指针找到所有解
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        //int[] nums = {-1, 0, 1, 2, -1, -4};
+        //int[] nums = {-4,-2,1,-5,-4,-4,4,-2,0,4,0,-2,3,1,-5,0};
+        int[] nums = {0, 0, 0};
+
+        List<List<Integer>> lists = solution.threeSum(nums);
+
+        System.out.println(lists);
+    }
+
+    /**
+     *
+     * 对于数组内的任意一个数e，都有数组内其他元素之和target等于-e，
+     * 那么这个数e就是符合题目要求的，这样就把三数之和，简化为了两数之和。
+     *
+     * 这题的难点在于，如何去重？分为以下几步：
+     *
+     * 1. 数组排序，目的在于一是让相同的数相邻，二是便于寻找target；
+     * 2. 遍历原数组，如果相邻的数是相同的，跳到下一个数；
+     * 3. 外层循环确定数e后，通过twoSum寻找两数之后为-e的即满足条件，将这三个数加到返回值列表中；
+     * 4. 符合条件的解可能不止一组，左指针前进，右指针后退，继续求和，注意，因为去重的原因，此时还需要判断左右指针的值以避免重复
+     *
+     * 时间复杂度：
+     *
+     * 排序 O(n㏒n)
+     * 搜索解 O(n²)
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> rtList = new ArrayList<>();
+        if (nums.length == 0) return rtList;
+
+        Arrays.sort(nums);
+        //System.out.println(Arrays.toString(nums));
+        // -4, -1, -1, 0, 1, 2
+        // -5, -5, -4, -4, -4, -2, -2, -2, 0, 0, 0, 1, 1, 3, 4, 4
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int target = -(nums[i]);
+                if (nums[left] + nums[right] == target) {
+                    rtList.add(Arrays.asList(-target, nums[left], nums[right]));
+                    left++;
+                    right--;
+                    while (left < nums.length && nums[left] == nums[left - 1]) left++;
+                    while (right > 0 && nums[right] == nums[right + 1]) right--;
+                } else if (nums[left] + nums[right] > target) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+        return rtList;
+    }
+}
